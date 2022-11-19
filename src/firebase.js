3dbbb96e274f1019a36f12f2dba750a2bdeb1059
Aringoaway <a.konshina.job@gmail.com>
2022-11-19 12:53:12 +0100

    remove unused files, add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAlximtuYeeh-a_f_98foOKsUFWrCVkhCg",
+	authDomain: "todo-app-with-firebase-bf7b7.firebaseapp.com",
+	projectId: "todo-app-with-firebase-bf7b7",
+	storageBucket: "todo-app-with-firebase-bf7b7.appspot.com",
+	messagingSenderId: "1091157625736",
+	appId: "1:1091157625736:web:57e3f8066a5654f2ba483d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
